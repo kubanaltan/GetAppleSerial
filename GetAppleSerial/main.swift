678b19a316f98765cabc3d9feb3322928bd7586b
Kuban Altan <kuban.altan@gmail.com>
2015-10-29 16:04:47 +0200

    Initial Commit

---
+++ GetAppleSerial/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GetAppleSerial
+//
+//  Created by kuban on 10/29/15.
+//  Copyright © 2015 Zero Density. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
